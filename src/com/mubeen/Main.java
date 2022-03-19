93afbfa3e3626c603bbe1c57e5252363083c5ef2
Mubeen <mdmubeen1307@gmail.com>
2022-03-19 11:29:00 +0530

    Car class with getters and setters

---
+++ src/com/mubeen/Main.java
@@ -0,0 +1,22 @@
+package com.mubeen;
+
+/*
+ *** Requirements  ***
+ * The dealership sells many types of cars.
+ * A car is identified by its make,price,year and color
+ * The dealership app should be able to search for cars based on make and price
+ * After a sale, the car must drive to the nearest exit
+ * Every car now has spare parts.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Car nissan = new Car("Nissan",12000, 2000,"Red");
+
+        System.out.println(nissan);
+
+    }
+}
